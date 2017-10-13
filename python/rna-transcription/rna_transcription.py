4dcb4819938b62a0c5145db530e630fdc068a118
mvi <mvi@mvi.me>
2017-10-13 09:08:35 +0200

    completes exercise 5: rna transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,15 @@
 def to_rna(dna_strand):
-    pass
+    # attention! order of the items in the tuples below is essential. do not change.
+    valid_dna_nucl = ('G', 'C', 'T', 'A')
+    valid_rna_nucl = ('C', 'G', 'A', 'U')
+    
+    rna_strand = ''
+    for nucl in dna_strand:
+        try:
+            i = valid_dna_nucl.index(nucl)
+            rna_strand += valid_rna_nucl[i]
+        except ValueError:
+           return '' 
+    return rna_strand
+
+
