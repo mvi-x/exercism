f8a16abf06a75fe54b145f579e5bfea9dab79537
mvi <mvi@mvi.me>
2017-10-13 09:23:36 +0200

    refactors exercise 5 (rna transcription)

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,14 +1,16 @@
 def to_rna(dna_strand):
-    # attention! order of the items in the tuples below is essential. do not change.
-    valid_dna_nucl = ('G', 'C', 'T', 'A')
-    valid_rna_nucl = ('C', 'G', 'A', 'U')
+    dna_to_rna_map = {
+            'G':'C',
+            'C':'G',
+            'T':'A',
+            'A':'U'
+            }
     
     rna_strand = ''
     for nucl in dna_strand:
         try:
-            i = valid_dna_nucl.index(nucl)
-            rna_strand += valid_rna_nucl[i]
-        except ValueError:
+            rna_strand += dna_to_rna_map[nucl]
+        except KeyError:
            return '' 
     return rna_strand
 
